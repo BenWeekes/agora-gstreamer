0fc977d07f0a311c336bc9ea69e4456ff9e749c8
Ben Weekes <benweekes73@gmail.com>
2021-11-11 16:23:39 +0000

    added  build_all_arm.sh

---
+++ build_all_arm.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+BUILD_DIR=$PWD
+
+#build libagorac
+echo "building and installing libagorac ..."
+cd agora/libagorac/ && ./install.sh $BUILD_DIR/agora/sdk/arm
+
+sudo rm $BUILD_DIR/gst-agora/plugin-src/meson.build || true
+cp $BUILD_DIR/gst-agora/plugin-src/arm.build $BUILD_DIR/gst-agora/plugin-src/meson.build
+
+echo "building and installing agora plugin ..."
+sudo rm -rf $BUILD_DIR/gst-agora/build || true
+cd $BUILD_DIR/gst-agora && meson build && ./install
+
