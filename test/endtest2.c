97ee102782e4ac583cbf3ce0af37ec40b2f04490
Ben Weekes <benweekes73@gmail.com>
2021-12-09 18:07:09 +0000

    update test apps

--- test/endtest2.c
+++ test/endtest2.c
@@ -1,5 +1,21 @@
 #include <gst/gst.h>
 
+
+void on_agora_iframe_request_fn(GstElement* object,gpointer user_data){
+
+     g_print("->Endtest: iframe requested!\n");
+}
+
+void on_agora_on_connecting_fn(GstElement* object,gpointer user_data){
+
+     g_print("->Endtest: on connecting!\n");
+}
+
+void on_agora_on_connected_fn(GstElement* object,gpointer user_data){
+
+     g_print("->Endtest: on connected!\n");
+}
+
 int main(int argc, char *argv[]) {
 
   GstElement *pipeline;
@@ -17,32 +33,6 @@ int main(int argc, char *argv[]) {
   pipeline = gst_parse_launch (
 
 	"videotestsrc pattern=ball is-live=true ! video/x-raw,format=I420,width=320,height=180,framerate=60/1 ! videoconvert ! x264enc key-int-max=60 tune=zerolatency ! agoraioudp appid=20b7c51ff4c644ab80cf5a4e646b0537 channel=test userid=123 ! fakesink sync=false"
-	//" videotestsrc pattern=ball is-live=true ! video/x-raw,format=I420,width=320,height=180,framerate=60/1 ! videoconvert ! x264enc key-int-max=60 tune=zerolatency ! filesink location=test.h264"
-     //"audiotestsrc num-buffers=1000 ! fakesink sync=false"
-	 // "videotestsrc pattern=ball is-live=true ! video/x-raw,format=I420,width=320,height=180,framerate=60/1 ! videoconvert ! agoraio ! fakesink sync=false"
-	 //"videotestsrc pattern=ball is-live=true ! video/x-raw,format=I420,width=320,height=180,framerate=60/1 ! videoconvert ! x264enc key-int-max=60 tune=zerolatency ! agoraioudp appid=20b7c51ff4c644ab80cf5a4e646b0537 channel=test userid=123 ! fakesink sync=false"
-	 //"videotestsrc pattern=ball is-live=true ! video/x-raw,format=I420,width=320,height=180,framerate=60/1 ! videoconvert ! x264enc key-int-max=60 tune=zerolatency ! agoraio appid=20b7c51ff4c644ab80cf5a4e646b0537 channel=test userid=123 ! fakesink sync=false"
-	// Without audio
-
-//	"v4l2src device=/dev/video0 ! "
-
-//	"agoraioudp  appid=ba68d9ec60fb45cb94254e0b4d01af92 channel=forme3 verbose=true ! "
-
-//	"queue ! h264parse ! vaapidecodebin ! "
-
-//	"autovideosink "
-
-	// Test with audio
-
-	/*"v4l2src device=/dev/video0 ! video/x-h264,width=1440,height=1920,format=byte-stream,framerate=[30/1,10000000/333333] ! queue ! "
-
-	"agoraioudp  appid=ba68d9ec60fb45cb94254e0b4d01af92 channel=forme3 outport=7372 inport=7373 verbose=true ! "
-
-	"queue ! h264parse ! vaapidecodebin ! autovideosink "
-
- 	"alsasrc device=sysdefault:CARD=CX20921 name=micsrc ! audio/x-raw,width=16,depth=16,rate=44100,channel=1 ! queue leaky=2 max-size-time=100000000 ! audioconvert ! audioresample quality=8 ! opusenc ! audio/x-opus,rate=48000,channels=1 ! udpsink host=127.0.0.1 port=7373 "
-
-        "udpsrc port=7372 ! audio/x-raw,format=S16LE,channels=1,rate=48000,layout=interleaved ! audioconvert ! queue name=1on1AudIn ! pulsesink volume=1.0 name=incaudsink"*/
 
 	,NULL);
 
@@ -50,6 +40,27 @@ int main(int argc, char *argv[]) {
 
 
   g_object_set (pipeline, "message-forward", TRUE, NULL);
+
+  GstElement *agoraioUdp = gst_bin_get_by_name(GST_BIN(pipeline), "agoraioudp0");
+  if(agoraioUdp!=NULL)
+  {
+		g_print (">>installing signals to agoraio\n");
+
+		//you ma not need all of them 
+		//on iframe request
+		g_signal_connect (agoraioUdp, "on-iframe",
+                    G_CALLBACK (on_agora_iframe_request_fn), NULL);
+
+        //on connecting
+		g_signal_connect (agoraioUdp, "on-connecting",
+                    G_CALLBACK (on_agora_on_connecting_fn), NULL);
+
+		//on connected
+		g_signal_connect (agoraioUdp, "on-connected",
+                    G_CALLBACK (on_agora_on_connected_fn), NULL);
+  }
+  
+
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
   /* Wait until error or EOS */
@@ -70,7 +81,7 @@ int main(int argc, char *argv[]) {
 
 		static int secs = 1;
 
-		if (secs == 1) {
+		if (secs == 2) {
 
 		        gst_element_set_state(pipeline, GST_STATE_PAUSED);
 
@@ -78,7 +89,7 @@ int main(int argc, char *argv[]) {
 
 		}
 
-		if (secs == 2) {
+		if (secs == 4) {
 
 		        gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
@@ -86,7 +97,7 @@ int main(int argc, char *argv[]) {
 
 		}
 
-		if (secs == 3) {
+		if (secs == 6) {
 
 		        gst_element_send_event(pipeline, gst_event_new_eos());
 				
