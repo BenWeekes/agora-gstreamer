a27c5870ea16fdc52c348b273b4945687f045ed8
Ben Weekes <benweekes73@gmail.com>
2021-12-13 20:11:01 +0000

    testing sdk with no shm

--- build_all.sh
+++ build_all.sh
@@ -4,7 +4,9 @@ BUILD_DIR=$PWD
 
 #build libagorac
 echo "building and installing libagorac ..."
-cd agora/libagorac/ && ./install.sh $BUILD_DIR/agora/sdk
+#cd agora/libagorac/ && ./install.sh $BUILD_DIR/agora/sdk
+cd agora/libagorac/ && ./install.sh $BUILD_DIR/agora/sdk_v43200
+
 
 echo "building and installing agora plugin ..."
 sudo rm -rf $BUILD_DIR/gst-agora/build || true
